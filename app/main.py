3b7cad942a5324b87ec5aaff0d162914f218702c
shailu-3126 <shailu8198@outlook.com>
2025-11-12 08:41:42 +0000

    Update backend message to test GitHub Actions deployment

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Hello from Terraform + GCP automated backend!")
+    return jsonify(message="Hello from Terraform + GCP automated backend! Updated! 🚀
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
